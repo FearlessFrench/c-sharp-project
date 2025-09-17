16b2694846b5a7e75908bb2de0f1e10f9d7deb77
周防 有希 <pippoopanu@hotmail.com>
2025-09-17 11:33:54 +0700

    Add files from Sep 17, 2025

---
+++ 38_array_of_objects/Program.cs
@@ -0,0 +1,43 @@
+using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            /*
+            Car[] garage = new Car[3];
+
+            Car car1 = new Car("Mustang");
+            Car car2 = new Car("Corvette");
+            Car car3 = new Car("Charger");
+
+            garage[0] = car1;
+            garage[1] = car2;
+            garage[2] = car3;
+
+            Console.WriteLine(garage[0].model);
+            Console.WriteLine(garage[1].model);
+            Console.WriteLine(garage[2].model);
+            */
+
+            Car[] garage = { new Car("Mustang"), new Car("Corvette"), new Car("Lambo") };
+
+            foreach (Car car in garage)
+            {
+                Console.WriteLine(car.model);
+            }
+
+            Console.ReadKey();
+        }
+    }
+    class Car
+    {
+        public String model;
+
+        public Car(String model)
+        {
+            this.model = model; 
+        }
+    }
+}
